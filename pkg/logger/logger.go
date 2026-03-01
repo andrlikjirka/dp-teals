d2410dfc276069efd1213a6e9b9ca178e6060669
andrlikjirka <andrlik.jirka@gmail.com>
2026-03-01 01:19:19 +0100

    Refactor logger implementation to use a Logger struct with enhanced functionality

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,36 +3,36 @@ package logger
 import (
 	"log/slog"
 	"os"
-	"sync"
 )
 
-var (
-	logger *slog.Logger
-	once   sync.Once
-)
+// Logger is a wrapper around slog.Logger that provides additional functionality
+type Logger struct {
+	*slog.Logger
+}
 
-func Init() {
-	once.Do(func() {
-		handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+// New creates a new Logger instance based on the environment.
+func New(env string) *Logger {
+	var handler slog.Handler
+
+	if env == "production" {
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 			Level: slog.LevelInfo,
 		})
-		logger = slog.New(handler)
+	} else {
+		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		})
+	}
 
-	})
+	return &Logger{
+		Logger: slog.New(handler),
+	}
 }
 
-func Info(msg string, args ...any) {
-	logger.Info(msg, args...)
-}
-
-func Error(msg string, args ...any) {
-	logger.Error(msg, args...)
-}
-
-func Debug(msg string, args ...any) {
-	logger.Debug(msg, args...)
-}
-
-func Warn(msg string, args ...any) {
-	logger.Warn(msg, args...)
+// WithComponent adds a "component" field to the logger for better context in logs.
+func (l *Logger) WithComponent(name string) *Logger {
+	// We return a new *logger.Logger so we don't mutate the original
+	return &Logger{
+		Logger: l.Logger.With(slog.String("component", name)),
+	}
 }
