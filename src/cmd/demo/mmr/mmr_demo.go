d0be2e4eb1fe5f7cba68b8d83f1547288a04697d
andrlikjirka <andrlik.jirka@gmail.com>
2026-03-07 22:50:47 +0100

    Update mmr_demo.go to use transaction data and add inclusion proof demonstration

--- src/cmd/demo/mmr/mmr_demo.go
+++ src/cmd/demo/mmr/mmr_demo.go
@@ -7,20 +7,24 @@ import (
 )
 
 func main() {
-	leaves := [][]byte{
-		[]byte("A"),
-		[]byte("B"),
-		[]byte("C"),
-		[]byte("D"),
-		[]byte("E"),
+	data := [][]byte{
+		[]byte("tx1"),
+		[]byte("tx2"),
+		[]byte("tx3"),
+		[]byte("tx4"),
+		[]byte("tx5"),
+		[]byte("tx6"),
+		[]byte("tx7"),
+		[]byte("tx8"),
+		[]byte("tx9"),
 	}
 
 	m := mmr.NewMMR(nil)
 	m.PrintSummary()
 
 	fmt.Println("\nAdding Leaves...")
-	for _, leaf := range leaves {
-		err := m.Append(leaf)
+	for _, d := range data {
+		err := m.Append(d)
 		if err != nil {
 			panic(err)
 		}
@@ -28,4 +32,24 @@ func main() {
 	m.PrintSummary()
 	m.PrintPeaks()
 	m.PrintTree()
+
+	demoInclusionProof(m, m.RootHash(), []byte("tx2"))
+}
+
+func demoInclusionProof(m *mmr.MMR, root []byte, targetData []byte) {
+	fmt.Printf("--- Testing Inclusion Proof for '%s' ---\n", string(targetData))
+
+	proof, err := m.GenerateInclusionProofByData(targetData)
+	if err != nil {
+		fmt.Printf("Error generating inclusion proof: %v\n", err)
+		return
+	}
+	fmt.Printf("Proof generated with %d siblings:\n", len(proof.Siblings))
+	for i, sibling := range proof.Siblings {
+		fmt.Printf("  Sibling %d: %x\n", i, sibling[:4])
+	}
+
+	valid := mmr.VerifyInclusionProof(targetData, proof, root, nil)
+	fmt.Printf("Inclusion proof valid: %v\n", valid)
+	fmt.Println()
 }
